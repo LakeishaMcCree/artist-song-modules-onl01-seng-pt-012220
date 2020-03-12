ba8155fcd36d23fe4c38c29e258ea74aa3a3343e
Lakeisha McCree <lmccree215@icloud.com>
2020-03-12 00:44:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -21,13 +21,13 @@ class Song
     @@songs
   end
 
-  def reset_all
-    self.all.clear
-  end
+  #def reset_all
+    #self.all.clear
+  #end
   
-  def self.count
-    self.all.count
-  end
+  #def self.count
+    #self.all.count
+  #end
 
   def artist=(artist)
     @artist = artist
