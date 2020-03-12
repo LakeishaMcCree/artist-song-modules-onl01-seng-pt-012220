87041d0dc94707b07eb8a26003026fc34b248460
Lakeisha McCree <lmccree215@icloud.com>
2020-03-12 00:13:23 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,17 +1,18 @@
 module Memorable
-  def self.reset_all
-    self.all.clear
+  module ClassMethods
+    def self.reset_all
+      self.all.clear
+    end
+  
+    def self.count
+      self.all.count
+    end 
+  
   end
-  
-  def self.count
-    self.all.count
-  end 
-  
-end
 
-module InstanceMethods
-  def initialize
-    self.class.all <<self
+  module InstanceMethods
+    def initialize
+      self.class.all <<self
+    end
   end
-end
 end
