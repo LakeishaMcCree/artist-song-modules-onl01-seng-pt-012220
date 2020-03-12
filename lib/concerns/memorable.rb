cbcc9e58253ae9e60aa392140acd33063130153a
Lakeisha McCree <lmccree215@icloud.com>
2020-03-12 00:19:56 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,26 +1,3 @@
 module Memorable
-  module ClassMethods
-    def self.reset_all
-      self.all.clear
-    end
-  
-    def self.count
-      self.all.count
-    end 
-  
-  end
 
-  module InstanceMethods
-    def initialize
-      self.class.all <<self
-    end
-  end
-end
-
-class Artist 
-  extend Memorable
-end
-
-class Song 
-  extend Memorable
 end
